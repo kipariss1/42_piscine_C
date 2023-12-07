8d89a6fea095643bf7376776b1639a14743520b8
kipariss1 <kpassudin@gmail.com>
2023-12-07 11:21:17 +0100

    Exersise04 finished

--- Day02/Exersise04/ft_print_comb.c
+++ Day02/Exersise04/ft_print_comb.c
@@ -6,16 +6,6 @@ void ft_putchar(char c)
   write(1, &c, 1);
 }
 
-void int_num_to_char_arr(int num, char res_char_arr[], int num_len)
-{
-	int ascii_offset = 48;
-
-	int devided_num[3] = {num/100, num%100/10, num%10};
-
-	for (int i; i<num_len; i++) {
-		res_char_arr[i] = devided_num[i] + ascii_offset; 
-	}	
-}
 
 void print_char_arr_by_one(char res_char_array[], int num_len)
 {
@@ -25,17 +15,32 @@ void print_char_arr_by_one(char res_char_array[], int num_len)
 		ft_putchar(res_char_array[i]);		
 	}
 
+	ft_putchar(',');
+	ft_putchar(' ');
+
 }
 
 void ft_print_comb(void)
 {
 	char res_char_arr[3];
 	int num_len = 3;
-	int in_val = 358;
+	int ascii_offset = 48;
 
-	int_num_to_char_arr(in_val, res_char_arr, num_len);
-	print_char_arr_by_one(res_char_arr, num_len);
-	// TODO: finish make algorithm for printing non repeating characters!
+	for (int i=0; i<10; i++) {
+		for (int j=0; j<10; j++) {
+			if ((j != i) && (j>i)) {
+				for (int k=0; k<10; k++) {
+					if ((k != j) && (k != i) && (k>j)) {
+						res_char_arr[0] = i+ascii_offset;
+						res_char_arr[1] = j+ascii_offset;
+						res_char_arr[2] = k+ascii_offset;
+						print_char_arr_by_one(res_char_arr, num_len);
+					}
+				}
+			}
+		}
+	}
+
 		
 }
 
