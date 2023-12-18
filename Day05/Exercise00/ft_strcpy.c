d2e825ee4a259efc8af29368e39948a3677c2f04
kipariss1 <kpassudin@gmail.com>
2023-12-18 23:13:33 +0100

    Day05 began, need to finish >:|

---
+++ Day05/Exercise00/ft_strcpy.c
@@ -0,0 +1,22 @@
+char* ft_strcpy(char *dest, char *src) {
+
+    int i;
+
+    for (i=0; src[i]!='\0'; i++) {
+        dest[i] = src[i];
+    }
+
+    dest[i+1] = '\0';
+
+   return dest; 
+}
+
+int main() {
+
+    char src[10] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', '\0'};
+    char dest[10] = {'\0'};
+
+    ft_strcpy(dest, src);
+
+    return 0;
+}
