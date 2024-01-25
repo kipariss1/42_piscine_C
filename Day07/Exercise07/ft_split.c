6ffb5ad01b984c800c6aa8ecdb7b2269cc5d47f9
kipariss1 <kpassudin@gmail.com>
2024-01-25 13:06:37 +0100

    Day07 is finished

---
+++ Day07/Exercise07/ft_split.c
@@ -0,0 +1,90 @@
+#include <stdlib.h>
+#include "libft.h"
+
+int is_separator(char ch, char *charset){
+
+    for (int i=0; charset[i]!='\0'; i++){
+        if (ch==charset[i]){
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+char** ft_split_whitespaces(char *str, char *charset){     
+
+    int n_words = 0;
+    for (int i=0; str[i]!='\0'; i++){
+        if (is_separator(str[i], charset)){
+            n_words++;
+        }
+    } 
+    n_words++;
+
+    int words_len[n_words];
+    int curr_word = 0;
+    int curr_word_len = 0;
+
+    for (int i=0; str[i]!='\0'; i++){
+        if (!is_separator(str[i], charset)){
+            curr_word_len++;
+        }
+        else if ((is_separator(str[i], charset)) && (curr_word_len==0)){
+            n_words--;
+            continue;
+        }
+        else{
+            words_len[curr_word] = curr_word_len;
+            curr_word++;
+            curr_word_len = 0;
+        } 
+    }
+
+    words_len[curr_word] = curr_word_len;
+
+    char **res = (char**)malloc((n_words+1)*sizeof(char*));
+
+    curr_word = 0;
+    int curr_word_i = 0;
+    int new_string_should_be_allocated_flag = 1;
+    for (int i=0; str[i]!='\0'; i++){
+        if (new_string_should_be_allocated_flag){
+            res[curr_word] = (char*)malloc((words_len[curr_word]+1)*sizeof(char));
+            new_string_should_be_allocated_flag = 0;
+        }
+
+        if (!is_separator(str[i], charset)){
+            res[curr_word][curr_word_i] = str[i];
+            curr_word_i++;
+        }
+        else if ((is_separator(str[i], charset)) && (curr_word_i==0)){
+            continue;
+        }
+        else {
+            res[curr_word][curr_word_i] = '\0';     // terminating the string 
+            curr_word++;
+            curr_word_i = 0;
+            new_string_should_be_allocated_flag = 1;
+        }
+
+    }
+
+    res[n_words+1] = NULL;
+
+    return res;
+}
+
+char** ft_split(char *str, char *charset){
+    return ft_split_whitespaces(str, charset);
+}
+
+int main(){
+
+    char test[] = "this is Test string, whIch will be spliTted";
+    char charset[] = "TI";
+
+    char **res = ft_split(test, charset);
+
+    return 0;
+}
---
+++ Day07/Exercise07/libft.h
@@ -0,0 +1,13 @@
+void ft_putchar(char c);
+void ft_putstr(char* str);
+char* ft_strcat(char *dest, char *src);
+int ft_strcmp(char* s1, char* s2);
+char* ft_strcpy(char *dest, char *src);
+unsigned int ft_strlcat(char *dest, char *src, unsigned int size);
+int ft_strlen(char *str);
+char* ft_strncat(char *dest, char *src, int n);
+char* ft_strncpy(char* dest, char* src, unsigned int n);
+void ft_swap(int *a, int *b);
+void ft_initstr(char *str, unsigned int strlen);
+int ft_iterative_power(int nb, int power);
+
