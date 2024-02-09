f75c61ddb285cf4748cea3f8668e2be51cac6bb1
kirill <kirill@linux.myguest.virtualbox.org>
2024-02-09 10:04:43 +0100

    Exercise13 done

--- Day09/Exercise13/ft_compact.c
+++ Day09/Exercise13/ft_compact.c
@@ -1,6 +1,4 @@
 int ft_compact(char **tab, int length){
-    // TODO: somehow bubble up nulls to the end
-
     int n_of_null_elements = 0;
     for (int i=0; i<length; i++){
         if (tab[i]==0){
@@ -11,19 +9,26 @@ int ft_compact(char **tab, int length){
         }
     }
 
+    for (int i=0; i<n_of_null_elements; i++){
+        tab[length-1-i]='\0';
+    }
+
     return length-n_of_null_elements;
 }
 
 int main(){
-
-    char *tab[5];
+    int len = 8;
+    char *tab[len];
     tab[0] = "test";
     tab[1] = 0;
     tab[2] = "test2";
     tab[3] = "test3";
     tab[4] = "test4";
+    tab[5] = 0;
+    tab[6] = "test5";
+    tab[7] = "test6";
 
-    int res = ft_compact(tab, 5);
+    int res = ft_compact(tab, len);
 
     return (0);
 }
