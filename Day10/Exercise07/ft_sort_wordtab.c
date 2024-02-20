38d9c8964298a5741c1f75e7439ef04b91bfd19b
kirill <kirill@linux.myguest.virtualbox.org>
2024-02-20 12:05:03 +0100

    Exercise07 started

---
+++ Day10/Exercise07/ft_sort_wordtab.c
@@ -0,0 +1,12 @@
+#include "ft_sort_wordtab.h"
+
+void ft_sort_wordtab(char **tab){
+
+}
+
+int main(){
+
+     
+
+    return (0);
+}
---
+++ Day10/Exercise07/ft_sort_wordtab.h
@@ -0,0 +1,7 @@
+#ifndef __FT_SORT_WORDTAB__
+#define __FT_SORT_WORDTAB__
+
+char** ft_split_whitespaces(char *str);
+void ft_sort_wordtab(char **tab);
+
+#endif // __FT_SORT_WORDTAB__
