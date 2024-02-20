7dff8e82af0c3e33176f351908480ca7389a7d70
kirill <kirill@linux.myguest.virtualbox.org>
2024-02-20 14:00:40 +0100

    Exercise07 done

--- Day10/Exercise07/ft_sort_wordtab.c
+++ Day10/Exercise07/ft_sort_wordtab.c
@@ -1,12 +1,35 @@
 #include "ft_sort_wordtab.h"
+#include "libft.h"
 
 void ft_sort_wordtab(char **tab){
 
+    int len_tab;
+    for (len_tab=0; tab[len_tab]!=0; len_tab++){}
+
+    for (int i=0; i<len_tab; i++){
+        for (int j=0; j<len_tab-1-i; j++){
+            if (ft_strcmp(tab[j], tab[j+1])>0){
+                char *temp = tab[j];
+                tab[j] = tab[j+1];
+                tab[j+1] = temp; 
+            }
+        } 
+    }
+
 }
 
-int main(){
+int main(int argc, char *argv[]){
 
-     
+    char **tab = ft_split_whitespaces(argv[1]);
+
+    ft_sort_wordtab(tab); 
+
+    for (int i=0; tab[i]!=0; i++){
+        ft_putstr(tab[i]);
+        ft_putstr(", ");
+    }
+
+    ft_putchar('\n');
 
     return (0);
 }
