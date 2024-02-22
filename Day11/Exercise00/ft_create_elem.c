189d9c7af0428b1850f8dbcb46eb71d711496d58
kirill <kirill@linux.myguest.virtualbox.org>
2024-02-22 23:59:46 +0100

    Exercise00 started

--- Day11/Exercise00/ft_create_elem.c
+++ Day11/Exercise00/ft_create_elem.c
@@ -3,10 +3,19 @@
 
 t_list* ft_create_elem(void *data){
 
+    t_list *new_list_el = (t_list*)malloc(sizeof(t_list));
+    new_list_el->data = data;
+    new_list_el->next = NULL;
+
+    return new_list_el;
 }
 
 int main(){
 
+    int a = 18;
+    char *b = "this is one type of data";
+    t_list* res = ft_create_elem((char*)b);
+    res->next = ft_create_elem((int*)&a);
 
     return (0);
 }
