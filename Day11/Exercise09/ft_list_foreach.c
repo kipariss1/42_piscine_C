b714aba8798676f3425b7d16890c461b36ce9fa4
kirill <kirill@linux.myguest.virtualbox.org>
2024-02-26 23:36:12 +0100

    Exercise09 done

--- Day11/Exercise09/ft_list_foreach.c
+++ Day11/Exercise09/ft_list_foreach.c
@@ -1,8 +1,8 @@
 #include "ft_list.h"
 #include <stdlib.h>
 
-void replace_first_char(char *str){
-    str[0] = '0';
+void replace_to_zero(int *digit){
+    *digit = 0;
 }
 
 void ft_list_foreach(t_list *begin_list, void (*f)(void*)){
@@ -10,6 +10,7 @@ void ft_list_foreach(t_list *begin_list, void (*f)(void*)){
     for (curr=begin_list; curr->next!=0x00; curr=curr->next){
         f(curr->data);
     }
+    f(curr->data);
 }
 
 void ft_list_push_back(t_list **begin_list, void *data){
@@ -31,13 +32,19 @@ void ft_list_push_back(t_list **begin_list, void *data){
 int main(){
 
     t_list **list_array = (t_list**)malloc(sizeof(t_list));
-    ft_list_push_back(list_array, "1 test data");
-    ft_list_push_back(list_array, "2 test data");
-    ft_list_push_back(list_array, "3 test data");
-    ft_list_push_back(list_array, "4 test data");
-    ft_list_push_back(list_array, "5 test data");
+    int a, b, c, d, e;
+    a = 1;
+    b = 2;
+    c = 3;
+    d = 4;
+    e = 5;
+    ft_list_push_back(list_array, &a);
+    ft_list_push_back(list_array, &b);
+    ft_list_push_back(list_array, &c);
+    ft_list_push_back(list_array, &d);
+    ft_list_push_back(list_array, &e);
 
-    ft_list_foreach(*list_array, &replace_first_char);
+    ft_list_foreach(*list_array, replace_to_zero);
 
     return (0);
 }
