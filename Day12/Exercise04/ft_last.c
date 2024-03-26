4913858475ef22d495e0513f3dcae7fc8ab2e4d5
kipariss1 <kpassudin@gmail.com>
2024-03-26 23:26:18 +0100

    continuing

--- Day12/Exercise04/ft_last.c
+++ Day12/Exercise04/ft_last.c
@@ -13,10 +13,17 @@
 
 /* month and days declaration */
 char *days[] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
-char *months[] = {"January", "February", "March", "April", "May", "June", 
-                    "July", "August", "September", "October", "November", "December"};
+char *months[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", 
+                    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
 int days_in_month[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 int initial_year = 1970;
+
+/* Double linked list of struct utmp's */
+struct utmplist {
+	struct utmp ut;
+	struct utmplist *next;
+	struct utmplist *prev;
+};
 
 /* last time formats */
 struct last_timefmt {
@@ -59,21 +66,24 @@ static struct last_timefmt short_fmt = {
 
 
 // Global vars
-long long int lastdate;
+static time_t lastdate;
+static time_t currentdate;
 
 int time_formatter(int fmt, char *dst, time_t *when){
 
     switch (fmt)
     {
     case LAST_TIMEFTM_HHMM:
+    {
         // convert "when" time to "- 20:27" format
+        // *when = *when + 3600;       // adding one hour, because my time is GMT+1
         int mins = (*when / 60) % 60;
         char mins_str[3] = {'\0'};
         ft_int_to_str(mins_str, mins, 2);
 
         int hours = (*when / 3600) % 24;
         char hours_str[3] = {'\0'};
-        ft_int_to_str(hours_str, hours, 2);
+        ft_int_to_str(hours_str, hours, 2);       
 
         char time_str[5] = {'\0'};
         ft_strcpy(time_str, hours_str);
@@ -83,11 +93,14 @@ int time_formatter(int fmt, char *dst, time_t *when){
         ft_strcpy(dst, time_str);
 
         break;
+    }
 
     case LAST_TIMEFTM_CTIME:
+    {
         // convert "when" time to "Sat Mar 23 20:27" fmt
+        // *when = *when + 3600;       // adding one hour, because my time is GMT+1
         int days_total = *when / 86400;
-        int curr_day_of_the_week = days_total%7;
+        int curr_day_of_the_week = (days_total+3)%7;
 
         int curr_day = 0;
         int curr_month = 0;
@@ -119,12 +132,51 @@ int time_formatter(int fmt, char *dst, time_t *when){
             curr_day++;
         }
 
-        // TODO: add curr day, month, day of month to dst
-        // TODO: add time to dst
+        curr_day++;     // due to it began with 0
+
+        // add time to dst
+        int mins = (*when / 60) % 60;
+        char mins_str[3] = {'\0'};
+        ft_int_to_str(mins_str, mins, 2);
+
+        int hours = (*when / 3600) % 24;
+        char hours_str[3] = {'\0'};
+        ft_int_to_str(hours_str, hours, 2);
+
+        char time_str[5] = {'\0'};
+        ft_strcpy(time_str, hours_str);
+        ft_strcpy(time_str+2, ":");
+        ft_strcpy(time_str+3, mins_str);
+
+        int dst_cursor = 0;
+        ft_strcpy(dst, days[curr_day_of_the_week]);
+        dst_cursor = dst_cursor + ft_strlen(days[curr_day_of_the_week]);
+
+        ft_strcpy(dst+dst_cursor, " ");
+        dst_cursor++;
+
+        ft_strcpy(dst+dst_cursor, months[curr_month]);
+        dst_cursor = dst_cursor + ft_strlen(months[curr_month]);
+
+        ft_strcpy(dst+dst_cursor, " ");
+        dst_cursor++;
+
+        // add curr_day
+        char *str_curr_day = ft_int_to_str_malloc(curr_day);
+        ft_strcpy(dst+dst_cursor, str_curr_day);
+        dst_cursor = dst_cursor + ft_strlen(str_curr_day);
+        free(str_curr_day);
+
+        ft_strcpy(dst+dst_cursor, " ");
+        dst_cursor++;
+
+        // TODO: add time_str
+        ft_strcpy(dst+dst_cursor, time_str);
 
         (void)curr_day_of_the_week;
 
         break;
+    }
     }
 
     return 1;
@@ -134,6 +186,8 @@ int list(struct utmp *p, time_t logout_time, int what){
 
     char logouttime[32] = {'\0'};    // str to store logout time
     char logintime[32] = {'\0'};     // str to store login time
+    char length[32] = {'\0'};
+    char final[512] = {'\0'};        // final str to print
 
     struct last_timefmt *fmt;
     time_t utmp_time, secs;
@@ -151,10 +205,46 @@ int list(struct utmp *p, time_t logout_time, int what){
     // put time in "in" format to logintime
 
     time_formatter(fmt->in_fmt, logintime, &utmp_time);
-
-    ft_strcpy(logouttime, "- ");
+ 
     // put time in "out" format to logouttime
-    time_formatter(fmt->out_fmt ,logouttime+2, &logout_time);
+    if (*logouttime){
+        ft_strcpy(logouttime, "- ");
+        time_formatter(fmt->out_fmt ,logouttime+2, &logout_time);
+    }
+
+    if (logout_time == currentdate){
+        ft_strcpy(logouttime+2, "  still running");
+    } else if (days) {
+        // add days to logouttime
+    } else if (hours) {
+        // add hours to logouttime
+    } else {
+        // add mins to logouttime
+    }
+
+    switch(what) {
+        // normal user-process, still logged in
+        case R_NOW:
+            ft_strcpy(logouttime, "   still");
+            ft_strcpy(length, "logged in");
+            break;
+    }
+
+    // printing out the FINAL line
+    ft_strcat(final, p->ut_user);
+    ft_strcat(final, "   ");
+    ft_strcat(final, p->ut_line);
+    ft_strcat(final, "   ");
+    ft_strcat(final, p->ut_host);
+    ft_strcat(final, " ");
+    ft_strcat(final, logintime);
+    ft_strcat(final, logouttime);
+    ft_strcat(final, " ");
+    ft_strcat(final, length);
+    ft_strcat(final, "\n");
+    ft_putstr(final);
+
+
 
     (void)what;
     (void)fmt;
@@ -172,10 +262,22 @@ int process_wtmp_file(){
     int quit = 0;       // flag for while cycle
 
     struct utmp ut;                         // current utmp entry
+    struct utmplist *ulist = NULL;        // all entries
+    struct utmplist *p;                     // pointer to ulist
+    struct utmplist *next;                  // pointer to next of ulist
     int size_utmp = sizeof(struct utmp);    // size of utmp struct
 
     time_t begintime;       // when wtmp begins
     time_t lastboot = 0;    // last boot time
+    time_t lastdown;        // last downtime
+    time_t lastrch;         // last run level change
+    int whydown = 0;
+
+    int c;
+
+    time(&lastdown);
+
+    lastdate = currentdate = lastrch = lastdown;
 
     int fd = open("/var/log/wtmp", O_RDONLY);
     if (fd == -1){
@@ -238,6 +340,22 @@ int process_wtmp_file(){
             break;
 
         case USER_PROCESS:
+            c = 0;
+            for (p = ulist; p; p = next){
+                next = p->next;
+                // TODO: finish work with ulist
+            }
+
+            if (c == 0) {
+                if (!lastboot){
+                    c = R_NOW;
+                }
+                else {
+                    c = whydown;
+                }
+                quit = list(&ut, lastboot, c);
+            }
+
             break;
 
         case DEAD_PROCESS:
